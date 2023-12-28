49f949c19e56db36d4022f67ea729b20e0aa76ae
joe-arkom <joeashef@gmail.com>
2023-12-28 14:42:30 +0100

    Add API service

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,27 +1,37 @@
 ﻿using Microsoft.AspNetCore.Components.WebView.Maui;
 using MauiBlazorApp.Data;
+using MauiBlazorApp.Services;
+using MauiBlazorApp.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace MauiBlazorApp;
-
-public static class MauiProgram
+namespace MauiBlazorApp
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
 
-		builder.Services.AddMauiBlazorWebView();
-		#if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
-		
-		builder.Services.AddSingleton<WeatherForecastService>();
 
-		return builder.Build();
-	}
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            builder.Services.AddTransient<IMyApiService, MyApiService>();
+            builder.Services.AddHttpClient<IMyApiService, MyApiService>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:your_api_port/");
+            });
+
+            return builder.Build();
+        }
+    }
 }
