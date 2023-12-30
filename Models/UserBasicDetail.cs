79634365673a09df92254c3cf0d896594ca7fa90
jozef-allen <joeashef@gmail.com>
2023-12-30 22:04:06 +0100

    Pull info from access token and put in SecureStorage

---
+++ Models/UserBasicDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBlazorApp.Models
+{
+    public class UserBasicDetail
+    {
+        public string Name { get; set; }
+        public string UserID { get; set; }
+        public string Token { get; set; }
+        public string Email { get; set; }
+    }
+}
